74019b2791191c4a338827d12d616b2a857acb50
Wenze0614 <wenze0614@gmail.com>
2021-12-26 09:59:37 +0800

    add more styles of subcriptionForm

--- khr-web/src/components/form/SubscriptionForm.tsx
+++ khr-web/src/components/form/SubscriptionForm.tsx
@@ -1,8 +1,9 @@
 import { Formik, FormikHelpers } from 'formik';
 import * as Yup from 'yup'
 import React from 'react';
-import { InputlArea } from './form-components';
+import { InputlArea } from '../ui/form-components';
 import Button from '../ui/Button';
+import styles from './SubscriptionForm.module.css'
 type subscriptionProps = {
     email: string
 }
@@ -14,7 +15,7 @@ const SubscriptionForm = () => {
                 email: Yup.string().email("please enter a valid email").required('email is required')
             })}
             onSubmit={(values: subscriptionProps, { setSubmitting }: FormikHelpers<subscriptionProps>) => { console.log(values) }}>
-            <form>
+            <form className={styles['subscription-form']}>
                 <InputlArea label='Email' className='text-input' name='email' type='email' placeholder=''></InputlArea>
                 <Button type="submit">Submit</Button>
             </form>
---
+++ khr-web/src/components/ui/form-components.tsx
@@ -0,0 +1,20 @@
+import {useField, FieldHookConfig } from 'formik';
+import React from 'react';
+import styles from './form-components.module.css'
+interface OtherProps{
+    label:string,
+    className:string
+}
+export const InputlArea = (props: OtherProps&FieldHookConfig<string>) => {
+    const [field, meta] = useField(props)
+    return (
+        <div className={styles[`${props.className}`]}>
+            <label htmlFor={props.id || props.name} >{props.label}</label>
+            <input  {...field} type={props.type} placeholder={props.placeholder}></input>
+            {meta.touched && meta.error ?
+                <div className={styles['error-message']}>{meta.error}</div> : null}
+        </div>
+    )
+}
+
+
