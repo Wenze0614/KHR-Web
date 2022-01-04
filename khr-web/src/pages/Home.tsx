b21a56baf5291d4e9912226f476cd34e5d6601a6
Wenze0614 <wenze0614@gmail.com>
2022-01-04 23:49:03 +0800

    add link to service cards

--- khr-web/src/pages/Home.tsx
+++ khr-web/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Strip from '../components/strip/Strip';
 import styles from './Home.module.css'
 import Card from '../components/ui/Card'
+import { Link } from 'react-router-dom';
 const Home = () => {
     console.log('home')
     return (
@@ -23,17 +24,20 @@ const Home = () => {
                         <h3>Donation</h3>
                         <p>All donations go towards providing the horses with quality care, to improve their lives both emotionally and physically.
                             KHR ensure the horses have the best opportunities to heal, recover, rehabilitate and to be re-homed to their perfect forever home.</p>
+                        <Link to="/donation">Learn More</Link>
                     </Card>
                     <Card className="service-card" backgroundImg={require('../assets/home/home-2.jpg')}>
                         <h3>Sponsorship</h3>
                         <p>You can help a horse through their recovery and rehabilitation.
                             All horses and ponies can be sponsored, your donations go towards caring for the horses to provide quality care, food, vet care, treatment, rehabilitation,
                             and training to ensure the horses are healthy, happy, and safe. </p>
+                        <Link to="/sponsorship">Learn More</Link>
                     </Card>
                     <Card className="service-card" backgroundImg={require('../assets/home/home-3.png')}>
                         <h3>Adoption</h3>
                         <p>All horses are adopted on a lifetime lease contract.
                             All adoption fees are donations to KHS, and no profit is made on the adoption of horses, all donations ensure KHS can continue our work to help horses in need.</p>
+                        <Link to="/adoption">Learn More</Link>
                     </Card>
                 </div>
             </Strip>
