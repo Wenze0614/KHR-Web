addb820553ed274533fd1837f1c316d294ce4bcf
Wenze0614 <wenze0614@gmail.com>
2022-01-04 23:12:24 +0800

    add sign up form

--- khr-web/src/components/form/SignUpForm.tsx
+++ khr-web/src/components/form/SignUpForm.tsx
@@ -7,17 +7,25 @@ import styles from './SignInForm.module.css'
 import Card from '../ui/Card'
 type logInProps = {
     email: string,
-    password: string
+    password: string,
+    reEnterPassword:string,
 }
 const SignInForm = () => {
 
     return (
         <Card className='signInForm-card' >
             <Formik
-                initialValues={{ email: '', password: '' }}
+                initialValues={{ email: '', password: '' , reEnterPassword:''}}
                 validationSchema={Yup.object({
                     email: Yup.string().email("please enter a valid email").required('email is required'),
-                    password: Yup.string().required("password is required")
+                    password: Yup.string().required("password is required"),
+                    reEnterPassword:Yup.string().when("password",{
+                        is:(val:string)=>(val && val.length > 0 ? true:false),
+                        then: Yup.string().oneOf(
+                            [Yup.ref("password")],
+                            "Both password need to be the same"
+                        )
+                    })
                 })}
                 onSubmit={(values: logInProps, { setSubmitting }: FormikHelpers<logInProps>) => {
 
@@ -27,7 +35,8 @@ const SignInForm = () => {
                     <p>Enter your email and password to login</p>
                     <InputlArea label='Email' className='text-input' name='email' type='email' placeholder=''></InputlArea>
                     <InputlArea label='Password' className='text-input' name='password' type='password' placeholder=''></InputlArea>
-                    <Button type="submit">Log In</Button>
+                    <InputlArea label='Confirm Password' className='text-input' name='reEnterPassword' type='password' placeholder=''></InputlArea>
+                    <Button type="submit">Sign Up</Button>
                 </form>
              </Formik>
 
--- khr-web/src/pages/SignIn.tsx
+++ khr-web/src/pages/SignIn.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignInForm from '../components/form/SignInForm'
+import SignUpForm from '../components/form/SignUpForm'
 import Strip from '../components/strip/Strip'
 import styles from './SignIn.module.css'
 export default function SignIn() {
+    const [isSignIn, setSignIn] = useState(true)
     return (
         <Strip background="home/home-3.png" darkness='0.3'>
-            <div className={styles.signIn}>
+            {isSignIn ? <div className={styles.signIn}>
                 <h2>Log in to our site</h2>
                 <SignInForm />
-            </div>
+                <p>Don't have an account? Click <span onClick={() => { setSignIn(false) }}>here</span> to sign up!</p>
+            </div> :
+                <div className={styles.signIn}>
+                    <h2>Sign Up to our site</h2>
+                    <SignUpForm />
+                    <p>Already have an account? Click <span onClick={() => { setSignIn(true) }}>here</span> to log in!</p>
+                </div>
+            }
+
         </Strip>
     )
 }
