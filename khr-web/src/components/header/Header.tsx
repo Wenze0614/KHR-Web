1429f01997270e115300e1c4cee212cb6100436f
Wenze0614 <wenze0614@gmail.com>
2022-01-12 12:24:33 +0800

    fade out NavBar styling

--- khr-web/src/components/header/Header.tsx
+++ khr-web/src/components/header/Header.tsx
@@ -1,8 +1,50 @@
 import Links from './Links';
 import styles from './Header.module.css'
+import { useState, useEffect } from 'react';
 const Header = () => {
+    const [show, setShow] = useState(true)
+    const [prevOffSet, setPrevOffSet] = useState(window.scrollY)
+    const handleScroll = () => {
+        console.log(window.pageYOffset)
+        setPrevOffSet(PrevOffSet => {
+            if (PrevOffSet > window.scrollY) {
+                console.log('scroll up')
+                setShow(true)
+            } else {
+                console.log('scroll down')
+                setShow(false)
+            }
+            return window.scrollY
+        })
+        // if (window.scrollY > currOffSet) {
+        //     setShow(false)
+        // } else {
+        //     console.log('scroll up')
+        //     setShow(true)
+
+        // }
+
+
+    }
+    //  useEffect(() => {
+    //     console.log(currOffSet)
+    //     if (window.scrollY > currOffSet) {
+    //         setShow(false)
+    //     } else {
+    //         console.log('scroll up')
+    //         setShow(true)
+
+    //     }
+    //     setCurrOffSet(window.scrollY)
+    // }, [currOffSet])
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
     return (
-        <header className={styles['header']}>
+        <header className={`${styles['header']} ${!show && styles.hide}`}>
             <img src={require('../../assets/logo.png')} alt="header-logo" className={styles.logo}></img>
             <Links></Links>
         </header>
