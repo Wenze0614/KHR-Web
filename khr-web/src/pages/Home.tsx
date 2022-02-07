2f2a89335cd5d854821eebb578e6a3a51127aba8
Wenze0614 <wenze0614@gmail.com>
2022-02-07 16:22:29 +0800

    change service card style

--- khr-web/src/pages/Home.tsx
+++ khr-web/src/pages/Home.tsx
@@ -18,26 +18,29 @@ const Home = () => {
                 <p>We help horses from Perth metro and regional areas.</p>
             </Strip>
             <Strip background="home/home-3.png" darkness='0.3'>
-                <h2>Our Services</h2>
+                {/* <h2>Our Services</h2> */}
                 <div className={styles['service-wraper']}>
                     <Card className="service-card" backgroundImg={require('../assets/home/home-1.jpg')}>
                         <h3>Donation</h3>
                         <p>All donations go towards providing the horses with quality care, to improve their lives both emotionally and physically.
-                            KHR ensure the horses have the best opportunities to heal, recover, rehabilitate and to be re-homed to their perfect forever home.</p>
-                        <Link to="/donation">Learn More</Link>
+                            KHR ensure the horses have the best opportunities to heal, recover, rehabilitate and to be re-homed to their perfect forever home.
+                            <Link to="/donation">Learn More</Link></p>
+
                     </Card>
                     <Card className="service-card" backgroundImg={require('../assets/home/home-2.jpg')}>
                         <h3>Sponsorship</h3>
                         <p>You can help a horse through their recovery and rehabilitation.
                             All horses and ponies can be sponsored, your donations go towards caring for the horses to provide quality care, food, vet care, treatment, rehabilitation,
-                            and training to ensure the horses are healthy, happy, and safe. </p>
-                        <Link to="/sponsorship">Learn More</Link>
+                            and training to ensure the horses are healthy, happy, and safe.
+                            <Link to="/sponsorship">Learn More</Link></p>
+
                     </Card>
                     <Card className="service-card" backgroundImg={require('../assets/home/home-3.png')}>
                         <h3>Adoption</h3>
                         <p>All horses are adopted on a lifetime lease contract.
-                            All adoption fees are donations to KHS, and no profit is made on the adoption of horses, all donations ensure KHS can continue our work to help horses in need.</p>
-                        <Link to="/adoption">Learn More</Link>
+                            All adoption fees are donations to KHS, and no profit is made on the adoption of horses, all donations ensure KHS can continue our work to help horses in need.
+                            <Link to="/adoption">Learn More</Link></p>
+
                     </Card>
                 </div>
             </Strip>
