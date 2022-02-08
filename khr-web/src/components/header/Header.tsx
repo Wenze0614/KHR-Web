65051774bde5873d202a53b3e94e2fafd2c9bd0e
Wenze0614 <wenze0614@gmail.com>
2022-02-08 19:45:19 +0800

    change header styling and add simple animation

--- khr-web/src/components/header/Header.tsx
+++ khr-web/src/components/header/Header.tsx
@@ -1,9 +1,50 @@
 import Links from './Links';
 import styles from './Header.module.css'
 import { useState, useEffect } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import { motion, AnimatePresence, useCycle } from 'framer-motion'
+
+const listsVar = {
+    open: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: .3,
+            ease: "easeOut"
+        }
+    },
+    close: {
+        opacity: 0,
+        y: -100,
+        transition: {
+            duration: .3,
+            ease: "easeOut"
+        }
+    }
+}
+const iconVar = {
+    enter: {
+        rotate: -180,
+        opacity: 1,
+        transition: {
+            duration: .3,
+            ease: "easeOut"
+        }
+    },
+    exit: {
+        rotate: 180,
+        opacity: 0,
+        transition: {
+            duration: .3,
+            ease: "easeOut"
+        }
+    }
+}
 const Header = () => {
     const [show, setShow] = useState(true)
     const [prevOffSet, setPrevOffSet] = useState(window.scrollY)
+    const [active, setActive] = useState(false)
     const handleScroll = () => {
         setPrevOffSet(PrevOffSet => {
             if (PrevOffSet > window.scrollY) {
@@ -11,7 +52,7 @@ const Header = () => {
             } else {
                 setShow(false)
             }
-            if(window.scrollY === 0){
+            if (window.scrollY === 0) {
                 setShow(true)
             }
             return window.scrollY
@@ -25,10 +66,32 @@ const Header = () => {
         }
     }, [])
     return (
+
         <header className={`${styles['header']} ${!show && styles.hide}`}>
             <img src={require('../../assets/logo.png')} alt="header-logo" className={styles.logo}></img>
-            <Links></Links>
+            <AnimatePresence exitBeforeEnter>
+                {active ?
+                    <motion.div key="open-button" className={styles.toggle} onClick={() => { setActive(active => !active); }}
+                        variants={iconVar}
+                        animate='enter'
+                        exit='exit'>
+                        <MenuOpenIcon style={{ color: "white" }} className={`${styles.menuIcon}`}></MenuOpenIcon>
+                    </motion.div> :
+                    <motion.div className={styles.toggle} onClick={() => { setActive(active => !active); }}
+                        variants={iconVar}
+                        animate='enter'
+                        exit='exit'>
+                        <MenuIcon style={{ color: "white" }} className={`${styles.menuIcon}`} ></MenuIcon>
+                    </motion.div>
+                }
+            </AnimatePresence>
+            <motion.div className={`${styles.menu} ${active ? styles.active : null}`}
+                variants={listsVar}
+                animate={active ? 'open' : 'close'}><Links></Links>
+            </motion.div>
+            <div className={styles.links}><Links></Links></div>
         </header>
+
     );
 };
 
