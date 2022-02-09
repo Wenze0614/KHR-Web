c2c93eb82625605187b99236505d72cb95cb7d33
Wenze0614 <wenze0614@gmail.com>
2022-02-09 11:17:31 +0800

    revised spelling mistake and changed footer size

--- khr-web/src/components/header/Links.tsx
+++ khr-web/src/components/header/Links.tsx
@@ -8,7 +8,7 @@ const Links = () => {
         <ul className={styles.links}>
             <li><NavLink to="/" className={(NavStatus) => NavStatus.isActive ? styles['active'] : ''}>Home</NavLink></li>
             <li><NavLink to="/donation" className={(NavStatus) => NavStatus.isActive ? styles['active'] : ''}>Donation</NavLink></li>
-            <li><NavLink to="/sponsorship" className={(NavStatus) => NavStatus.isActive ? styles['active'] : ''}>Sponsorshipt</NavLink></li>
+            <li><NavLink to="/sponsorship" className={(NavStatus) => NavStatus.isActive ? styles['active'] : ''}>Sponsorship</NavLink></li>
             <li><NavLink to="/adoption" className={(NavStatus) => NavStatus.isActive ? styles['active'] : ''}>Adoption</NavLink></li>
             {/* {authCtx.isLoggedIn ?
                 <li><NavLink to="/profile" className={(NavStatus) => NavStatus.isActive ? styles['active'] : ''}>{authCtx.user.username}</NavLink></li>
